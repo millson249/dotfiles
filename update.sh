e618f4cbbcac9355749fbee98e37ea823d5c0aab
Miles Simpson <millson249@gmail.com>
2015-02-07 09:45:05 -0800

    Added missing submodule pull to update.sh

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ git pull
 git submodule foreach git clean -df
 git submodule update --init --remote
 git submodule foreach git checkout master
+git submodule foreach git pull
 # Janus update
 cd ~/dotfiles/vim
 rake
