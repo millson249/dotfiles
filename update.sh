6744c9de3d94557c976cffc453a0fe4a50cf584f
Miles Simpson <miles.a.simpson@gmail.com>
2020-03-20 18:26:59 -0700

    Updated submodule reference, Updated updater script

--- update.sh
+++ update.sh
@@ -4,8 +4,6 @@ cd ~/dotfiles
 git pull
 git submodule foreach git clean -df
 git submodule update --init --remote
-git submodule foreach git checkout master
-git submodule foreach git pull
 # Janus update
 cd ~/dotfiles/vim
 rake
